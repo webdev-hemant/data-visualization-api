044a581c1fd3ce117e2479623e9d8b7f71ea9511
Hemant Jadhav <webdev.hemant@example.com>
2023-04-13 15:19:43 +0530

    error handler changes

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -4,15 +4,25 @@ const connectDb = () => {
   mongoose
     .connect(process.env.MONGODB_CONNECTION, {
       dbName: process.env.DB_NAME,
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
     })
-    .then((db) => {
-      const { host, name } = db.connection;
-      console.log(`DB=${name} & HOST=${host}`);
+    .then(() => {
+      console.log(`mongodb conected`);
     })
-    .catch((err) => {
-      console.log({ err });
-      // process.exit(1);
-    });
+    .catch((err) => console.log({ err }));
+  // mongoose
+  //   .connect(process.env.MONGODB_CONNECTION, {
+  //     dbName: process.env.DB_NAME,
+  //   })
+  //   .then((db) => {
+  //     const { host, name } = db.connection;
+  //     console.log(`DB=${name} & HOST=${host}`);
+  //   })
+  //   .catch((err) => {
+  //     console.log({ err });
+  //     // process.exit(1);
+  //   });
 };
 
 module.exports = connectDb;
